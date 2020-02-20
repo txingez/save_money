5211b8a48b82a1bcdbf65243f67f265805d56523
Thai Hung Do <hung_dt@septeni-technology.jp>
2020-02-20 18:07:48 +0700

    commit for build login page

--- src/entry.js
+++ src/entry.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 import './main.scss'
 import { store } from './store/store'
 import { router } from './router/router'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
 
 new Vue({
     el: '#app',
